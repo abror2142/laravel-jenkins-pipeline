1a6f217e85bd4c16171968fc51a2935a8885695d
Abrorbek Ismoilov <107830282+abror2142@users.noreply.github.com>
2025-05-18 16:48:54 +0500

    entrypoint.sh: Making production ready

--- php/entrypoint.sh
+++ php/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 mkdir -p database
 touch ./database/database.sqlite
 
-php artisan migrate:fresh --seed --force
+php artisan migrate:fresh
 
 chown -R www-data:www-data /app /app/vendor /app/storage
 chmod -R 775 /app /app/vendor /app/storage
